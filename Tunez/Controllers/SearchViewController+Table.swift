0770ed1e7d13f866ebf9a938ffa4621b59814349
Ricky Memije <rickymemije@gmail.com>
2023-04-09 18:25:35 -0600

    Init

---
+++ Tunez/Controllers/SearchViewController+Table.swift
@@ -0,0 +1,45 @@
+//
+//  SearchViewController+Table.swift
+//  Tunez
+//
+//  Copyright © 2018 WOZ-U. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - UITableViewDataSource
+
+extension SearchViewController: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if shouldFilterResults() {
+			return filteredResults.count
+		}
+        return searchResults.count
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: MediaCell.Cell.ident, for: indexPath) as? MediaCell  else {
+			fatalError("The dequeued cell is not an instance of MediaCell.")
+		}
+
+		cell.delegate = self
+		let mediaItem = shouldFilterResults() ? filteredResults[indexPath.row] : searchResults[indexPath.row]
+		cell.configureCell(with: mediaItem)
+
+		return cell
+    }
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let mediaItem = searchResults[indexPath.row]
+		if mediaItem.downloaded { playMediaItem(mediaItem) }
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+}
